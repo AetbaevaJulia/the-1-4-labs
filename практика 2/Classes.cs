bbd0f4e40e1d155341405eea518a79578f7e557c
AetbaevaJulia <aetbaevajulia05@gmail.com>
2023-11-12 16:03:23 +0500

    создание классов

---
+++ практика 2/Classes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практика_2
+{
+    internal class Token
+    {
+
+    }
+
+    internal class Parenthesis: Token
+    {
+
+    }
+    internal class Number: Token
+    {
+        
+    }
+    internal class Operation: Token
+    {
+
+        public int Priority (char symbol)
+        {
+            if (symbol == '(' || symbol == ')') return 0;
+            else if (symbol == '+' || symbol == '-') return 1;
+            else return 2;
+        }
+    }
+}
