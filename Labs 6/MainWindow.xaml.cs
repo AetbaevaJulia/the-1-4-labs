e04ad0613ef07a40e78eded2764b1aba9bf1d26f
AetbaevaJulia <aetbaevajulia05@gmail.com>
2024-03-20 07:55:38 +0500

    123

--- Labs 6/MainWindow.xaml.cs
+++ Labs 6/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace Labs_6
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
